3c4a882856e9ce4d92646197891a0e35b0fa4d16
baburello <babur565@gmail.com>
2024-01-16 16:33:28 +0500

    feat: set date and time

--- js/app.js
+++ js/app.js
@@ -17,9 +17,7 @@ let todos = JSON.parse(localStorage.getItem("list"))
     ? JSON.parse(localStorage.getItem("list"))
     : [];
 
-
-
-if (todos.length) showTodos()
+if (todos.length) showTodos();
 
 //setToDos to Local Storage
 
@@ -27,16 +25,42 @@ function setTodos() {
     localStorage.setItem("list", JSON.stringify(todos));
 }
 
+//time
+
+function getTime() {
+    const now = new Date();
+    const date = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+    const month =
+        now.getMonth() < 10 ? "0" + (now.getMonth() + 1) : now.getMonth();
+    const year = now.getFullYear();
+
+    const hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+    const minute = now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+    const second = now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
+
+    const month_title = now.getMonth()
+    fullDay.textContent = `${date} ${months[month_title]}, ${year}`
+
+    hourEl.textContent = hour
+    minuteEl.textContent = minute
+    secondEl.textContent = second
+
+    return(`${hour}:${minute}, ${date} ${month} ${year}`);
+}
+
+setInterval(getTime, 1000)
+
 //show todos
 
 function showTodos() {
     const todos = JSON.parse(localStorage.getItem("list"));
-    listGroupTodo.innerHTML = ''
+    listGroupTodo.innerHTML = "";
     todos.forEach((item, index) => {
         listGroupTodo.innerHTML += `<li class="list-group-item d-flex justify-content-between">
         ${item.text}
         <div class="todo-icons">
-            <span class="opacity-50 me-2">16.01.2024</span>
+            <span class="opacity-50 me-2">${item.time}</span>
             <img
                 src="./img/edit.svg"
                 alt="edit icon"
@@ -72,11 +96,11 @@ formCreate.addEventListener("submit", (e) => {
     if (todoText.length) {
         todos.push({
             text: todoText,
-            time: "14:40, 16.01.2024",
+            time: getTime(),
             completed: false,
         });
         setTodos();
-        showTodos()
+        showTodos();
     } else {
         showMessage("message-create", "Please, enter some todo...");
     }
